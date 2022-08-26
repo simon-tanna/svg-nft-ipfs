545e890bdb187f1588313df505a8a91c98a113ea
simon-tanna <gcab022109@coderacademy.edu.au>
2022-08-26 11:08:35 +1000

    added comment change in function

--- contracts/SvgNft.sol
+++ contracts/SvgNft.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SvgNft {
-    constructor() {
-        
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract SvgNft is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+
+    constructor(address vrfCoordinator) VRFConsumerBaseV2(vrfCoordinator) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
     }
 
-    function requestNFT() public {
+    function requestNFT() public {}
 
-    }
-
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {
-
-    }
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function tokenURI(uint256) public {
-        
+        // making change here
     }
 }
